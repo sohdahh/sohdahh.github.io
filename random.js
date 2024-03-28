cf8eb0ed9f853aeb4fbdac5799a6b8783d48d929
Sohdah <sohdahvirtual@gmail.com>
2024-03-28 13:07:35 -0700

    Useless feature

--- random.js
+++
@@ -1,8 +0,0 @@
-function genRand(min, max, decimalPlaces) {  
-    var rand = Math.random()*(max-min) + min;
-    var power = Math.pow(10, decimalPlaces);
-    return Math.floor(rand*power) / power;
-}
-
-document.body.style.setProperty('--rand1', Math.random(genRand(0, 40, 0)));
-document.body.style.setProperty('--rand2', Math.random(genRand(0, 40, 0)));
